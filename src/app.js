e1f69dc92f9bf533ed572ec00ff7a8307bee1b1e
Alvin Galit <aroblesgalit@gmail.com>
2019-01-03 21:15:55 -0800

    installed validator and imported it

--- src/app.js
+++ src/app.js
@@ -1,15 +1,2 @@
-// import './utils.js';
-// import subtract, { square, add } from './utils.js';
-
-// console.log('app.js is running!');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 20));
-
-import isSenior, { isAdult, canDrink } from './person.js';
-console.log(isAdult(17));
-console.log(canDrink(18));
-console.log(isSenior(64));
-
-// Setting the default export and function
-// Grab the default and call it
+// install -> import -> use
+import validator from 'validator';
