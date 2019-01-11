e2aec5e021db7583511add4e2fe655c687d986a2
Alvin Galit <aroblesgalit@gmail.com>
2019-01-10 20:39:01 -0800

    installed babel plugin transform class properties

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
@@ -12,5 +13,9 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_modules/
         }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
     }
 };
