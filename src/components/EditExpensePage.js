f83da91d3738b3c31ff302cb3035871cc0ddfa3b
Alvin Galit <aroblesgalit@gmail.com>
2019-01-21 22:57:49 -0800

    testing for github

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -4,6 +4,7 @@ const EditExpensePage = (props) => {
     console.log(props);
     return (
         <div>
+            Testing for github.
             Editing the expense with id of {props.match.params.id}
         </div>
     );
